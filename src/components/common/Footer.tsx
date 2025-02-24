c4f89e50565cf828bbba72218ac8c5f6ae4f744a
Abdullah Fitri Che Wan <ftk2431@gmail.com>
2025-02-25 00:53:20 +0800

    fix: footer styling

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 left-0 right-0 p-2 bg-white/80 backdrop-blur-sm">
-      <div className="container mx-auto flex justify-center items-center gap-2 text-sm text-gray-600">
-        <span>Dibina oleh</span>
+      <div className="container mx-auto flex justify-center items-center text-sm text-gray-600">
+        <span className="flex gap-2">
+          Built with
+          <Image width={40} height={40} src="/next.svg" alt="Next.js" /> by:
+        </span>
         <div className="inline-block transform hover:scale-110 hover:-rotate-3 transition duration-300">
           <Link
             href="https://fvtrx.com"
